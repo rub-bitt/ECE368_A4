d4a9ca65fefe20ae0ff0dc1779118d126e9fce33
rub-bitt <rwoudneh@purdue.edu>
2024-09-25 10:47:12 -0400

    sub 5

--- a4.c
+++ a4.c
@@ -47,12 +47,7 @@ void close_window(Node **head, int window_num) {
 void switch_window(Node **head, int window_num){
     //bring chosen window_num to the front and delete former position
     close_window(head, window_num);
-    printf("\nswitch close");
-    print_front(*head);
     open_window(head, window_num);
-    printf("\nswitch open");
-    print_front(*head);
-
 }
 
 void print_front (Node *head){
